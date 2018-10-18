40b1b905a2e3cbeec3cb182d895bdf42ef66af66
Carl Smith <carlfsmithiii@gmail.com>
2018-10-18 12:33:19 -0400

    altered to use pug

--- app.js
+++ app.js
@@ -4,21 +4,26 @@ const fs = require('fs');
 const path = require('path');
 
 const port = 3000;
+const uploadsPath = './public/uploads';
+const publicPath = './public/';
 
 const app = express();
-app.use(express.static('./public'));
+app.set('view engine', 'pug');
+app.use(express.static(publicPath));
 // const upload = multer({ dest: './public/uploads/'});
 const storage = multer.diskStorage({
-    destination: './public/uploads', 
-    filename: function(req, file, callback) {
+    destination: uploadsPath,
+    filename: function (req, file, callback) {
         callback(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
     }
 });
 
 const upload = multer({
     storage: storage,
-    limits: {fileSize: 1000000},
-    fileFilter: function(req, file, callback) {
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter: function (req, file, callback) {
         checkFileType(file, callback);
     }
 });
@@ -35,18 +40,15 @@ function checkFileType(file, callback) {
 }
 
 app.get('/', (req, res) => {
-    
-    fs.readdir('./public/uploads', function (err, items) {
-        let responseString = '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><meta http-equiv="X-UA-Compatible" content="ie=edge"><title>Kenziegram</title></head><body> <section><form method="POST" action="uploads/" enctype="multipart/form-data"><div> <label for="image-upload">Upload an image</label> <input type="file" id="image-upload" name="myImage"></div><div> <button type="submit">Upload Image</button></div></form> </section><section id="images-container"></section> <script src="index.js"></script> </body></html>';
-
-        items.forEach(item => responseString += `<img src="uploads/${item}" width=500>`);
-        res.send(`${responseString}`);
+    fs.readdir(uploadsPath, function(err, items) {
+        const itemPaths = items.map(item => `uploads/${item}`);
+        res.render('index', {title: 'KenzieGram', h1: 'Welcome to Kenziegram', images: itemPaths});
     });
-
 });
 
 app.post('/uploads/', upload.single('myImage'), (req, res) => {
-    res.send(`<a href="..">Back</a><img src="${req.file.filename}">`);
+    // res.send(`<a href="..">Back</a><img src="${req.file.filename}">`);
+    res.render('upload', {title: 'Upload', h1: 'file uploaded', imagePath: `uploads/${req.file.filename}`});
 });
 
 app.listen(port, () => console.log(`Server listening on port ${port}.`));
